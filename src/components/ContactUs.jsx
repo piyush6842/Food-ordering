90dfeeffb87063e358b5c235d0800bf5803b4713
Piyush Mittal <95648531+piyush6842@users.noreply.github.com>
2024-04-16 15:24:35 +0530

    ContactUs.jsx

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const ContactUs = ()=>{
     return(
         <div className="contact-page">
-            <h1>Contact Us</h1>
+            <h1><img className="w-screen h-screen" src="https://img.freepik.com/free-photo/upside-down-soda-chinese-food_23-2148242508.jpg?t=st=1713260536~exp=1713264136~hmac=037e37f53b022a14f9270d5e91e92306ad9a7b65c4eb6a438cb2e1b0841092d5&w=740"></img></h1>
         </div>
     )
 }
