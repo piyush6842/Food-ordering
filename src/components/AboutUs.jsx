81e57cb2f4eb624c8dbf968f5a3def97fbfdd733
Piyush Mittal <95648531+piyush6842@users.noreply.github.com>
2024-04-16 15:25:20 +0530

    AboutUs.jsx

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -3,8 +3,11 @@ import UserClass from "./UserClass";
 const AboutUs = ()=>{
     return(
         <div className="about-page">
-            <h1>About Us</h1>
-            <UserClass />
+            <h1><img className="w-screen h-100%"
+                     src="https://img.freepik.com/free-photo/street-chinese-food-with-copy-space_23-2148242502.jpg?t=st=1713260222~exp=1713263822~hmac=789dcd7d489384ffbe98d5246fdbdd50a58906d616c294e387a1bb977f8543b7"
+                     alt= "About us Page">
+                </img></h1>
+            
         </div>
     )
 }
